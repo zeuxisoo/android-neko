736bb50f7f7cc6690707b5d0903108b93e8a59b3
Zeuxis Lo <seekstudio@gmail.com>
2014-11-12 19:05:22 +0800

    add action process button for sign in button

--- app/src/main/java/im/after/app/ui/LoginActivity.java
+++ app/src/main/java/im/after/app/ui/LoginActivity.java
@@ -1,11 +1,21 @@
 package im.after.app.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.dd.processbutton.iml.ActionProcessButton;
 
 import im.after.app.R;
 
 
 public class LoginActivity extends BaseActivity {
+
+    private static final String TAG  = "LoginActivity";
+
+    EditText editTextUsername;
+    EditText editTextPassword;
+    ActionProcessButton buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,23 @@ public class LoginActivity extends BaseActivity {
         this.getActionBar().setDisplayShowTitleEnabled(false);
 
         this.setContentView(R.layout.activity_login);
+
+        this.editTextUsername = (EditText) this.findViewById(R.id.editTextUsername);
+        this.editTextPassword = (EditText) this.findViewById(R.id.editTextPassword);
+
+        this.buttonSignIn = (ActionProcessButton) this.findViewById(R.id.buttonSignIn);
+        this.buttonSignIn.setMode(ActionProcessButton.Mode.ENDLESS);
+        this.buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonSignIn.setEnabled(false);
+                editTextUsername.setEnabled(false);
+                editTextPassword.setEnabled(false);
+
+                // Start progress animation (Start at 0 will not show animation)
+                buttonSignIn.setProgress(1);
+            }
+        });
     }
 
 }
