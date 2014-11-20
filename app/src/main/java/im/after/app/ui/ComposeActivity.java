ef77b174c75d2209ba393768eb15e7119a7fbdac
Zeuxis Lo <seekstudio@gmail.com>
2014-11-20 17:00:22 +0800

    show user info in compose activity

--- app/src/main/java/im/after/app/ui/ComposeActivity.java
+++ app/src/main/java/im/after/app/ui/ComposeActivity.java
@@ -1,8 +1,16 @@
 package im.after.app.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.makeramen.RoundedTransformationBuilder;
+import com.squareup.picasso.Picasso;
+
+import im.after.app.AppContext;
 import im.after.app.R;
+import im.after.app.entity.bean.UserBean;
 
 public class ComposeActivity extends BaseActivity {
 
@@ -12,10 +20,32 @@ public class ComposeActivity extends BaseActivity {
     public static final int TYPE_MEMO    = 2;
     public static final int TYPE_TALK    = 3;
 
+    private ImageView imageViewComposeUserAvatar;
+    private TextView textViewComposeUsername;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compose);
+
+        this.setContentView(R.layout.activity_compose);
+
+        this.imageViewComposeUserAvatar = (ImageView) this.findViewById(R.id.imageViewComposeUserAvatar);
+        this.textViewComposeUsername    = (TextView) this.findViewById(R.id.textViewComposeUsername);
+
+        this.setUserInfo();
+    }
+
+    private void setUserInfo() {
+        UserBean userBean = ((AppContext) this.getApplicationContext()).getUserBean();
+
+        if (userBean != null) {
+            Picasso.with(this).load(userBean.getAvatar()).fit().transform(
+                new RoundedTransformationBuilder().borderColor(R.color.compose_user_avatar_border).borderWidthDp(1).cornerRadiusDp(30).oval(false).build()
+            ).into(this.imageViewComposeUserAvatar);
+
+            this.textViewComposeUsername.setText(userBean.getUsername());
+            this.imageViewComposeUserAvatar.setBackgroundColor(Color.TRANSPARENT);
+        }
     }
 
 }
