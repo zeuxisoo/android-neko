8aaa5d83817a854c224fe241170ff02e0aa10c3d
Zeuxis Lo <seekstudio@gmail.com>
2016-05-18 16:14:59 +0800

    make SplashActivity is main launcher

--- app/src/main/java/im/after/neko/mvp/view/login/activity/SplashActivity.java
+++ app/src/main/java/im/after/neko/mvp/view/login/activity/SplashActivity.java
@@ -1,13 +1,15 @@
-package im.after.neko;
+package im.after.neko.mvp.view.login.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import im.after.neko.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
