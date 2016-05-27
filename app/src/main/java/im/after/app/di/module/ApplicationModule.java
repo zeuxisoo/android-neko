315951f1374e66f681e770c7794ab6d45232eb28
Zeuxis Lo <seekstudio@gmail.com>
2016-05-27 11:33:33 +0800

    bump OkHttp to 3.3.0 to fix not support http2 problem

--- app/src/main/java/im/after/app/di/module/ApplicationModule.java
+++ app/src/main/java/im/after/app/di/module/ApplicationModule.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import com.google.gson.Gson;
 
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
@@ -13,7 +12,6 @@ import dagger.Module;
 import dagger.Provides;
 import im.after.app.MyApplication;
 import okhttp3.OkHttpClient;
-import okhttp3.Protocol;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 @Module
@@ -40,13 +38,7 @@ public class ApplicationModule {
                 .connectTimeout(30, TimeUnit.SECONDS)
                 .readTimeout(30, TimeUnit.SECONDS)
                 .addInterceptor(httpLoggingInterceptor);
-
-        // Disable http2 for OkHttp to fix current issue
-        // - Ref: https://forums.bitfire.at/topic/1082/nginx-http-2-incompatible-with-okhttp
-        // - Ref: https://trac.nginx.org/nginx/ticket/959
-        // - Ref: https://github.com/square/okhttp/issues/2543
-        builder.protocols(Arrays.asList(Protocol.HTTP_1_1, Protocol.SPDY_3));
-
+        
         return builder.build();
     }
 
